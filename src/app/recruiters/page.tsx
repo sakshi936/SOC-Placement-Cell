ac31659cd747bc07dcc13c0ed67946b363f66ab7
sakshipandey2601 <pandeysakshi2601@gmail.com>
2024-11-06 22:41:35 +0530

    feat: route pages

---
+++ src/app/recruiters/page.tsx
@@ -0,0 +1,11 @@
+export default function page() {
+	return (
+		<div className=" flex flex-col justify-center items-center p-4 h-screen gap-y-10">
+			<h1 className="text-5xl font-semibold text-center">Recruiters</h1>
+			<p className="w-1/2 text-center">
+				Lorem ipsum dolor, sit amet consectetur adipisicing elit. Doloribus non esse facere sequi quos! Nulla minus iusto est illum architecto magni quisquam, ea eligendi dolorum maiores
+				minima. Cum, repellat aperiam.
+			</p>
+		</div>
+	);
+}
