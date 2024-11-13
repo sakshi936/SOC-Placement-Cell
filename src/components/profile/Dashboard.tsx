9030f0025a003fe5e88c11fbed8c96854db077c5
prince-1908 <princesinghchouhan7470@gmail.com>
2024-11-14 02:44:08 +0530

    form and dashboard component

--- src/components/profile/Dashboard.tsx
+++ src/components/profile/Dashboard.tsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import { Navbar } from '@/components/Navbar'
-import { Footer } from './Footer';
+import { Footer } from '@/components/Footer';
 import { useState, useEffect, ChangeEvent } from 'react';
 
 type UserProfile = {
