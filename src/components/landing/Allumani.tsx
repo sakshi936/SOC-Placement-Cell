42294d3af687acc9ce194deda701c9ba806a0806
prince-1908 <princesinghchouhan7470@gmail.com>
2024-11-17 15:14:06 +0530

    allumni data in infinite scroll

--- src/components/landing/Allumani.tsx
+++ src/components/landing/Allumani.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import React, { useEffect, useState } from "react";
+import React from "react";
 import { InfiniteMovingCards } from "../ui/infinite-moving-cards";
 import { Button } from "../ui/button";
 import Link from "next/link";
@@ -10,8 +10,8 @@ export function Allumani() {
 	return (
 		<div className=" rounded-md flex flex-col gap-y-10 antialiased  items-center justify-center relative overflow-hidden">
 			<h2 className="text-heading ">Allumani</h2>
-			<div className="bg-slate-100 py-10 ">
-				<InfiniteMovingCards items={allumniesDetails} direction="right" speed="slow" />
+			<div className="py-10">
+				<InfiniteMovingCards items={allumniesDetails} direction="left" speed="slow" />
 			</div>
 			<Link href="/allumni" className="flex justify-center items-center">
 				<Button>See More...</Button>
--- src/components/ui/infinite-moving-cards.tsx
+++ src/components/ui/infinite-moving-cards.tsx
@@ -1,7 +1,7 @@
 "use client";
 
 import { cn } from "@/lib/utils";
-import { Link } from "lucide-react";
+// import { Link } from "lucide-react";
 import React, { useEffect, useState } from "react";
 
 export const InfiniteMovingCards = ({
@@ -12,9 +12,11 @@ export const InfiniteMovingCards = ({
 	className,
 }: {
 	items: {
-		quote: string;
-		name: string;
-		title: string;
+		name: string,
+		course: string,
+		session: string,
+		designation: string,
+		company: string
 	}[];
 	direction?: "left" | "right";
 	speed?: "fast" | "normal" | "slow";
@@ -66,7 +68,7 @@ export const InfiniteMovingCards = ({
 	};
 	return (
 		<div ref={containerRef} className={cn("scroller relative z-20  max-w-7xl overflow-hidden", className)}>
-			<ul ref={scrollerRef} className={cn(" flex min-w-full shrink-0 gap-4 py-4 w-max flex-nowrap", start && "animate-scroll ", pauseOnHover && "hover:[animation-play-state:paused]")}>
+			<ul ref={scrollerRef} className={cn("flex min-w-full shrink-0 gap-4 py-4 w-max flex-nowrap", start && "animate-scroll ", pauseOnHover && "hover:[animation-play-state:paused]")}>
 				{items.map((item, idx) => (
 					<li
 						className="w-[350px] max-w-full relative rounded-2xl border border-b-0 flex-shrink-0 border-slate-700 px-8 py-6 md:w-[450px] bg-blue-300"
@@ -74,15 +76,18 @@ export const InfiniteMovingCards = ({
 							// background: "linear-gradient(180deg, var(--slate-800), var(--slate-900)",
 							background: "linear-gradient(90deg, rgba(6,32,100,1) 0%, rgba(2,29,101,1) 50%, rgba(1,23,79,1) 100%)",
 						}}
-						key={item.name}
+						key={idx}
 					>
 						<blockquote>
 							<div aria-hidden="true" className="user-select-none -z-1 pointer-events-none absolute -left-0.5 -top-0.5 h-[calc(100%_+_4px)] w-[calc(100%_+_4px)]"></div>
-							<span className=" relative z-20 text-sm leading-[1.6] text-gray-100 font-normal">{item.quote}</span>
+							<span className=" relative z-20 text-2xl leading-[1.6] text-gray-100 font-semibold">
+								<p>{item.name}</p>
+								<p className="font-normal text-xl">{item.company} - {item.designation}</p>
+							</span>
 							<div className="relative z-20 mt-6 flex flex-row items-center">
 								<span className="flex flex-col gap-1">
-									<span className=" text-sm leading-[1.6] text-gray-400 font-normal">{item.name}</span>
-									<span className=" text-sm leading-[1.6] text-gray-400 font-normal">{item.title}</span>
+									<span className=" text-sm leading-[1.6] text-gray-400 font-normal">{item.course}</span>
+									<span className=" text-sm leading-[1.6] text-gray-400 font-normal">{item.session}</span>
 								</span>
 							</div>
 						</blockquote>
