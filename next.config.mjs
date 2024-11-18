f5d7e0bf3cbea1644a72b2abfe0b0597c463885f
sakshipandey2601 <pandeysakshi2601@gmail.com>
2024-11-18 19:08:00 +0530

    fix: placed student component ui

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,16 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
 
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'images.pexels.com',
+        port: '',
+        // pathname: '/account123/**',
+      },
+    ],
+  },
     async rewrites() {
         return [
           {
